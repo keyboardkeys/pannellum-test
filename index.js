83488a326b151bc83992193f0afdc905bc5a3447
Sebastian Obido <mail.sebastian.obido@gmail.com>
2026-09-08 14:18:37 +0800

    Update old commit

--- index.js
+++ index.js
@@ -2,3 +2,41 @@ pannellum.viewer('panorama', {
     "type": "equirectangular",
     "panorama": "https://pannellum.org/images/alma.jpg"
 });
+
+pannellum.viewer('panorama2', {
+    // "hotSpotDebug": true,
+    "default": {
+        "firstScene": "outside",
+        "author": "P Diddy",
+        "sceneFadeDuration": 1000
+    },
+    "scenes": {
+        "inside": {
+            "title": "Inside the House",
+            "panorama": "./interior.jpg",
+            "hotSpots": [{
+                "pitch": -2.1,
+                "yaw": 132.9,
+                "type": "scene",
+                "text": "Outside",
+                "sceneId": "outside"
+            }]
+        },
+        "outside": {
+            "title": "Outside",
+            "type": "equirectangular",
+            "panorama": "./house.jpg",
+            "hotSpots": [
+                {
+                    "pitch": -21.6,
+                    "yaw": 149.7,
+                    "type": "scene",
+                    "text": "Inside the House",
+                    "sceneId": "inside",
+                    "targetYaw": -23,
+                    "targetPitch": 2
+                }
+            ]
+        }
+    }
+});
